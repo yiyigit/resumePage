4479fe4a964eb39c3714ccd02dcd2554e2c404b9
yiyiingit <jerryyiyi@126.com>
2020-08-20 20:04:50 -0700

    initialize google analytics

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import './App.css';
-import ResumeHeader from './components/ResumeHeader';
-import ResumeContent from './components/resume-contents/ResumeContent';
-import Grid from '@material-ui/core/Grid';
+import React, { useEffect } from "react";
+import "./App.css";
+import ResumeHeader from "./components/ResumeHeader";
+import ResumeContent from "./components/resume-contents/ResumeContent";
+import Grid from "@material-ui/core/Grid";
+import ReactGA from "react-ga";
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize("UA-175987546-1");
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
-      <Grid container justify="center">
-          <Grid container item xs={4}>
-            <ResumeHeader />
-          </Grid>
-          <Grid container item xs={8}>
-            <ResumeContent />
-          </Grid> 
+    <Grid container justify="center">
+      <Grid container item xs={4}>
+        <ResumeHeader />
       </Grid>
+      <Grid container item xs={8}>
+        <ResumeContent />
+      </Grid>
+    </Grid>
   );
 }
 
